bceb343bb865905f9e1846f4605910501f849578
Uolyana Moner <98236055+uolyanaMoner@users.noreply.github.com>
2025-11-24 19:25:08 +0200

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
+    react(),
     tailwindcss(),
   ],
+  base: './',       // مهم جداً عشان Deployment على Vercel
+  build: {
+    outDir: 'dist', // المجلد النهائي بعد build
+  },
 })

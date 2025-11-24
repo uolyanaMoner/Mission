6755a8730b6c2e9badc39310896322736dd6ef2a
Uolyana Moner <98236055+uolyanaMoner@users.noreply.github.com>
2025-11-24 19:46:47 +0200

    Ready for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: './',       // مهم جدًا لكل المسارات النسبية
+  plugins: [react(), tailwindcss()],
+  base: './',       // مهم جدًا → يجعل كل ملفات JS وCSS relative
   build: {
-    outDir: 'dist', // مجلد Build النهائي
+    outDir: 'dist', // المجلد النهائي بعد build
   },
 })
